34f88f4466d68221a99a6cb1212bcf349db381be
Farzaneh Fallahpour <farzanehhh.fl@gmail.com>
2021-05-23 21:09:27 +0430

    add negative and positive number Btn

--- script.js
+++ script.js
@@ -8,24 +8,13 @@ const clearAllButton = document.querySelector('.row-button__key--clearAll');
 const equalButton = document.querySelector('.row-button__equal');
 const inputHeader = document.querySelector('.calculator__header__number-input');
 const outputHeader = document.querySelector('.calculator__header__number-output');
+const negetiveOrPositiveBtn = document.querySelector('.row-button--NegetiveOrPositive');
 let save = 0;
 let data = {
     opration: [],
     resulte: []
 }
 
-function nnn() {
-    let save0 = changeStringToNumberExprission(data.resulte.slice(0, -2).join(''));
-    let save1 = data.resulte.pop();
-    document.querySelector('.calculator__header__number-input').value = save0;
-    data.opration = [];
-    data.resulte = [];
-    data.opration.push(save0);
-    data.resulte.push(save0, save1);
-    console.log()
-    document.querySelector('.calculator__header__number-output').value = data.resulte.join('');
-    data.opration = [];
-}
 /* ..........................................................addEvent........................................... */
 
 numberButton.forEach(element => {
@@ -39,7 +28,7 @@ backspaceButton.addEventListener('click', deleteNumber)
 clearButton.addEventListener('click', clearResulteNumber)
 clearAllButton.addEventListener('click', clearResulteAndOprationNumber)
 equalButton.addEventListener('click', calculate)
-
+negetiveOrPositiveBtn.addEventListener('click', negativeOrPositiveNumber)
 
 /* ...........................................................function......................................................... */
 
@@ -92,21 +81,22 @@ function deleteNumber() {
         data.resulte.pop();
         document.querySelector('.calculator__header__number-output').value = data.resulte.join('');
         document.querySelector('.calculator__header__number-input').value = data.opration.join('');
+        console.log('2')
     }
     console.log(data.resulte, data.opration);
 }
 
 function clearResulteNumber() {
     data.opration = [];
-    document.querySelector('.calculator__header__number-input').value = data.opration.join('');
+    document.querySelector('.calculator__header__number-input').value = 0;
     console.log(data.opration);
 }
 
 function clearResulteAndOprationNumber() {
     data.resulte = [];
-    data.opration = [0];
+    data.opration = [];
     document.querySelector('.calculator__header__number-output').value = data.resulte.join('');
-    document.querySelector('.calculator__header__number-input').value = data.opration.join('');
+    document.querySelector('.calculator__header__number-input').value = 0;
     console.log(data.opration);
 }
 
@@ -120,13 +110,26 @@ function calculate() {
 
 /*..............................function independent to button...................................... */
 
+function checkOutputResult() {
+    let save0 = changeStringToNumberExprission(data.resulte.slice(0, -2).join(''));
+    let save1 = data.resulte.pop();
+    document.querySelector('.calculator__header__number-input').value = save0;
+    data.opration = [];
+    data.resulte = [];
+    data.opration.push(save0);
+    data.resulte.push(save0, save1);
+    console.log()
+    document.querySelector('.calculator__header__number-output').value = data.resulte.join('');
+    data.opration = [];
+}
+
 function inputResult() {
     document.querySelector('.calculator__header__number-input').value = data.opration.join('');
 }
 
 function outputResult() {
     if (data.resulte.includes('=')) {
-        nnn();
+        checkOutputResult();
     } else {
         document.querySelector('.calculator__header__number-output').value = data.resulte.join('');
         data.opration = [];
@@ -136,7 +139,7 @@ function outputResult() {
 
 function outputResult2() {
     if (data.resulte.includes('=')) {
-        nnn();
+        checkOutputResult();
     } else {
         document.querySelector('.calculator__header__number-output').value = data.resulte.join('');
         let save0 = data.resulte.slice(0, -1).join('');
@@ -144,3 +147,19 @@ function outputResult2() {
         data.opration = [];
     }
 }
+
+/* ..............................Negative number Or Positive number................................. */
+
+function negativeOrPositiveNumber() {
+    let saveResulte = data.opration.join('');
+    saveResulte = (-saveResulte);
+    data.resulte.splice(data.resulte.length - data.opration.length , data.opration.length)
+    data.opration = [];
+    data.resulte.push(saveResulte);
+    document.querySelector('.calculator__header__number-input').value  = saveResulte;
+    console.log(data.opration,data.resulte)
+}
+
+// var fruits = ["Banana", "Orange", "Apple", "Mango"];
+// fruits.splice(fruits.length - 2, 2);
+// console.log(fruits);
